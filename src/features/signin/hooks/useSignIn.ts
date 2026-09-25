2431230db577b59b651e40ef5227fba883443856
visilanti <silvianurrobianti@gmail.com>
2026-09-25 10:59:52 +0700

    fix: comment for temporary usesignin

--- src/features/signin/hooks/useSignIn.ts
+++ src/features/signin/hooks/useSignIn.ts
@@ -1,42 +1,42 @@
-"use client";
+// "use client";
 
-import { useMutation, useQueryClient } from "@tanstack/react-query";
-import { signIn as nextAuthSignIn } from "next-auth/react";
-import { useRouter } from "next/navigation";
+// import { useMutation, useQueryClient } from "@tanstack/react-query";
+// import { signIn as nextAuthSignIn } from "next-auth/react";
+// import { useRouter } from "next/navigation";
 
-interface SignInCredentials {
-    email: string;
-    password: string;
-}
+// interface SignInCredentials {
+//     email: string;
+//     password: string;
+// }
 
-export function useSignIn() {
-    const router = useRouter();
-    const queryClient = useQueryClient();
+// export function useSignIn() {
+//     const router = useRouter();
+//     const queryClient = useQueryClient();
 
-    return useMutation({
-        mutationFn: async ({ email, password }: SignInCredentials) => {
+//     return useMutation({
+//         mutationFn: async ({ email, password }: SignInCredentials) => {
 
-            const result = await nextAuthSignIn("credentials", {
-                email,
-                pwd: password,
-                redirect: false,
-            });
+//             const result = await nextAuthSignIn("credentials", {
+//                 email,
+//                 pwd: password,
+//                 redirect: false,
+//             });
 
-            if (!result || result.error || !result.ok) {
-                const errorMessage =
-                    result?.error && result.error !== "CredentialsSignin"
-                        ? result.error
-                        : "Invalid email or password.";
-                throw new Error(errorMessage);
-            }
+//             if (!result || result.error || !result.ok) {
+//                 const errorMessage =
+//                     result?.error && result.error !== "CredentialsSignin"
+//                         ? result.error
+//                         : "Invalid email or password.";
+//                 throw new Error(errorMessage);
+//             }
 
-            return result;
-        },
-        onSuccess: () => {
-            console.log("Login success");
-            queryClient.clear();
-            router.refresh();
-            router.push("/");
-        },
-    })
-}
+//             return result;
+//         },
+//         onSuccess: () => {
+//             console.log("Login success");
+//             queryClient.clear();
+//             router.refresh();
+//             router.push("/");
+//         },
+//     })
+// }
